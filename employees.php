680141cab1151cc05d25d63badd8fd0bb91c6c3d
Gavin Meren <gavin.meren@gmail.com>
2020-12-10 19:34:52 -0800

    Option menus for employee page, both employee and manager views, completed. Still need to implement functionality. - GM

--- employees.php
+++ employees.php
@@ -26,13 +26,15 @@
 	$con = $obj->connect();
 
 	$status = "";
+
+	$src = 'pin.php';
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
-	<meta name="viewpoet" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<link rel="stylesheet" href="employees.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -42,6 +44,16 @@
 
 	<!-- Latest compiled JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<script type="text/JavaScript">
+	$(document).ready(function() {
+		var currentEmployeePermissions = '<?php echo $_SESSION['currentEmployeePermissions']; ?>';
+		if (currentEmployeePermissions == 2) {
+			$("#idBtnViewEmployees").hide();
+			$("#idBtnManageEmployees").hide();
+			$(".form").css("height", "180px");
+		}
+	});
+	</script>
 </head>
 
 <body>
@@ -52,6 +64,20 @@
 		<button onclick="document.location='logout.php'" type="button" id="idBtnAboutus" class="btn btn-link">Logout</button>
 	</div>
 </header>
+
+	<div class="container">
+		<div class="formDescription">
+			<h1 class="description"><?= "Employee Page"?></h1>
+		</div>
+
+		<div class="form">
+			<input id="idBtnClock" type="submit" value="Clock-In">
+			<input type="submit" value="Clock-Out"> 
+			<input type="submit" value="Personal Employee Info">
+			<input id="idBtnViewEmployees" type="submit" value="View Employees">
+			<input id="idBtnManageEmployees" type="submit" value="Manage Employee Data">
+		</div>
+	</div>
 
 </body>
 </html>

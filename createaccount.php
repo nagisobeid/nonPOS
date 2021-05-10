c903f7f4b7c6f6d102356d0fffa15f12d9c2710e
kmitra36 <cyberzero36@hotmail.com>
2021-05-09 22:57:22 -0700

    modified php and css to files

--- createaccount.php
+++ createaccount.php
@@ -104,7 +104,7 @@ error_reporting(E_ALL);
     <header>
         <div id="main-bar">
             <img id="logo" src="./images/logo2.png"></img>
-            <button onclick="document.location='home.php'" type="button" id="idBtnHome"
+            <button onclick="document.location='index.php'" type="button" id="idBtnHome"
                 class="btn btn-link">Home</button>
             <button onclick="document.location='about.php'" type="button" id="idBtnAboutus" class="btn btn-link">About Us</button>
         </div>

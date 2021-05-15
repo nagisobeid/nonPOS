56f5dbb931027bdb5a25dbf6682e1e4b20e10df3
josh-logue <jlogue951@gmail.com>
2021-05-15 16:55:50 -0700

    Update owners.sql
    added token field

--- sql/owners.sql
+++ sql/owners.sql
@@ -7,5 +7,6 @@ CREATE TABLE IF NOT EXISTS owners(
     password varchar(75) NOT NULL,
     bName varchar(25) NOT NULL,
     tax decimal(5,2) DEFAULT 0.00,
+    token varchar(50) NULL,
     PRIMARY KEY (bID)
 );

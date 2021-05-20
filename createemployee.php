97daf5b8e53b53053fa8c65c00c31280c4bc063c
Nagi Obeid <nobeid@odin.cs.csubak.edu>
2021-05-19 20:43:49 -0700

    isolated css files into their own directory

--- createemployee.php
+++ createemployee.php
@@ -146,7 +146,7 @@ error_reporting(E_ALL);
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	<link rel="stylesheet" href="createemployee.css">
+	<link rel="stylesheet" href="./css/createemployee.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 	<!-- jQuery library -->

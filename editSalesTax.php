10c078755ed6010aea17d64d0e06c0378e52d804
Maribel Mireles <mmireles@csub.edu>
2021-05-19 17:14:20 -0700

    path changed for css files

--- editSalesTax.php
+++ editSalesTax.php
@@ -40,7 +40,7 @@
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	<link rel="stylesheet" href="editSalesTax.css">
+	<link rel="stylesheet" href="./css/editSalesTax.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 	<!-- jQuery library -->
